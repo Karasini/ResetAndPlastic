ed9647be8caf175da88e413dd3449784dbaaad7a
Mateusz <>
2019-01-23 18:46:16 +0100

    sub merge

--- ResetAndPlastic/Program.cs
+++ ResetAndPlastic/Program.cs
@@ -12,7 +12,6 @@ namespace ResetAndPlastic
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello from Plastic SCM");
-            Console.WriteLine("Just change it - presentation edit from plastic");
             Console.ReadKey();
         }
     }
